c163c562d372c3feed7598776533dedcb677b629
Brett Ponsler <ponsler@gmail.com>
2017-07-05 21:23:44 +0000

    Updated to use cmake 3.5 added asserts

---
+++ include/ros2_console/assert.hpp
@@ -0,0 +1,12 @@
+#ifndef ROS2_CONSOLE_ASSERT_HPP
+#define ROS2_CONSOLE_ASSERT_HPP
+
+
+#include <stdlib.h>
+
+
+#define ROS_ASSERT(expr) if (expr) { abort(); }
+#define ROS_BREAK() abort();
+
+
+#endif  // ROS2_CONSOLE_ASSERT_HPP
